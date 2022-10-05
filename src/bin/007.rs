8ee19997bf3e3f84b84aa870db8108ca0afd6288
kana-rus <kanarus786@gmail.com>
2022-10-05 19:33:49 +0900

    AC: 570ms 13484KB

--- src/bin/007.rs
+++ src/bin/007.rs
@@ -12,18 +12,12 @@ fn main() {
   
   for student_rate in b {
     match a.binary_search(&student_rate) {
-      Ok(_) => println!("{}", 0),
-      Err(index) => {
-        if index == 0 {
-          println!("{}", a[0] - student_rate);
-        } else if index == n {
-          println!("{}", student_rate - a[n-1]);
-        } else {
-          println!("{}",
-            (a[index] - student_rate).min(student_rate - a[index-1])
-          )
-        } 
-      }
+      Ok(_) => println!("0"),
+      Err(j) => println!("{}", match j {
+          i if i==n => student_rate - a[n-1],
+          0 => a[0] - student_rate,
+          _ => (a[j] - student_rate).min(student_rate - a[j-1]),
+      })
     }
   }
 }
